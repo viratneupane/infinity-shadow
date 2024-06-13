170ce7ba4f55322ba70c4f820020c4c203255729
viratneupane <167765135+viratneupane@users.noreply.github.com>
2024-06-13 16:18:58 +0545

    Update made

--- cryptography.py
+++ cryptography.py
@@ -2,14 +2,14 @@
 #in such a way that the given alphabet will be changed in refrence to the alphabet 6 steps ahead of it 
 # and gets * or $ sign based on the odd or even index of the alphabets and numbers will be 
 # similarly  converted into * or $ based on the odd or even number.
-def cr():
+
+def encrypted():
     from newpy import recog
     print(' This is the cryptography machine!')
     _a=input('\nenter the information that should be encrypted:')
     _b= _a.lower()
     _c="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
     z=[]
-    _z=[]
     for i in _b:
         if i in _c and _c.index(i)%2==0:
          y=_c.index(i)
@@ -23,16 +23,27 @@ def cr():
          z.append(i)
     t=''.join(z)
     print(f' The encrypted version of the given plain text is: {t} ')
-    print('\n Now we can decrypt that encrypted code')
-    for j in t:
-      if j=='*' or j=='$':
-        _u=0
-      elif j in _c: 
-        _y= _c.index(j)
-        _x= _c[_y-6]
-        _z.append(_x)
-      else:
-        _z.append(j)
-      _t=''.join(_z)
-    print(f'The decrypted version of the encrypted text is {_t}')
-cr()
+encrypted()
+print('\n Now we can decrypt that encrypted code!')
+
+def decrypted():
+ inp=input('To perform the decryption of the code, Please enter the private key::')
+ _inp=inp.lower()
+ if _inp=='@passkey$':
+  _z=[]
+  q=input('enter the code that needs to be decrypted: ')
+  __c='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+  for j in q:
+        if j=='*' or j=='$':
+          _u=0
+        elif j in __c: 
+          _y= __c.index(j)
+          _x= __c[_y-6]
+          _z.append(_x)
+        else:
+          _z.append(j)
+        _t=''.join(_z)
+  print(f'The decrypted version of the encrypted text is: {_t}')
+ else:
+   cr=0
+decrypted()
