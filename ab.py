b4d7c2aad67c085ef4272e96cab7d4e1b01894c5
Birat <birat@Birats-Laptop.local>
2025-01-28 22:34:27 +0545

    added the file ab.py

---
+++ ab.py
@@ -0,0 +1,17 @@
+d={'class': {"A":34,"B":5,"C":5}  }
+print(d["class"]['A']
+# https://www.cybrary.it/
+
+jumbled_list=[ ['oongoam','angom','mmanggo'],
+              ['orrrngea','rangeo','oorange' ],
+              ['anabaa','baanaaa','ananab']
+]
+correct_list=['mango','orange','banana']
+
+for i in range(len(correct_list)):
+    correct_item=correct_list[i]
+    sublist= jumbled_list[i]
+    for item in sublist:
+        if len(correct_item)==len(item):
+            print(correct_item,item)
+            break
